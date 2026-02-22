f2a4511629988eda7bd1f6afe128921ae5d0a023
Nishant Tripathi <n9169n@gmail.com>
2026-02-22 20:51:48 +0530

    CreatePage Fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
-import CreatePage from "./pages/Temp";
+import CreatePage from "./pages/CreatePage";
 import NoteDetailPage from "./pages/NoteDetailPage";
 import toast from "react-hot-toast";
 
