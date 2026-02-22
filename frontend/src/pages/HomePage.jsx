8d1c9fb6ab7dc5a7b6abc4a0d0ca9b1c97f32576
Nishant Tripathi <n9169n@gmail.com>
2026-02-22 20:51:02 +0530

    Fixing names

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import Navbar from "../Components/Navbar";
 import RateLimitedUI from "../Components/RateLimitedUI";
+import axios from "axios";
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = axios.get("http://localhost:5001/api/notes");
+        console.log(res.data);
+      } catch (error) {
+        console.log("Error fetching the notes, check fetchNotes");
+      }
+    };
+
+    fetchNotes();
+  }, []);
 
   return (
     <div className="min-h-screen">
